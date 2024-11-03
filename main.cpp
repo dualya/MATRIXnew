b612075326c7a37d175b97e9a5710722b6ba73b8
alina <duhaninaalina3@gmail.com>
2024-11-03 16:02:15 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -1,2 +1,27 @@
+#include <iostream>
+#include <stdexpect>
+
 int main()
-{}
+{
+  size_t m = 0, n = 0;
+  std::cin >> m >> n;
+  if (!(std::cin >> m >> n))
+  {
+    std::cerr << "Invalid matrix dimensions\n";
+    return 1;
+  }
+  try
+  {
+    Matrix mat(m, n);
+    mat.input(std::cin)
+    mat.print(std::cout);
+    mat.resize(m + 1, n + 1);
+    mat.fill(0);
+    mat.print(std::cout);
+  }
+  catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+  return 0;
+}
